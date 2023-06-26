4653eb24eaacff9c07ab1da04fe6e1becaf2f3af
ridham <ridhamsood1@gmail.com>
2023-06-26 17:56:05 +0530

    Backend api for Movie

---
+++ src/main/java/com/ridham/movies/entity/Review.java
@@ -0,0 +1,39 @@
+package com.ridham.movies.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "reviewIds")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId id;
+
+    private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
---
+++ src/main/java/com/ridham/movies/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.ridham.movies.repository;
+
+import com.ridham.movies.entity.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
